f203695a2f5c3dd5554e9ccbeb96299c3a15cb1d
Elliot NPWR <elliot@localhost.localdomain>
2015-09-04 17:28:09 +0100

    memccpy

---
+++ ft_memccpy.c
@@ -0,0 +1,15 @@
+#include "libft.h"
+
+void	*ft_memccpy(void *dst, void *src, int c, size_t n)
+{
+	unsigned int	i;
+
+	i = -1;
+	while (++i < n)
+	{
+		((unsigned char *)(dst))[i] = ((unsigned char *)(src))[i];
+		if (((unsigned char *)(dst))[i] == (unsigned char)(c))
+			return ((unsigned char *)(dst) + i);
+	}
+	return (NULL);
+}
--- libft.h
+++ libft.h
@@ -2,6 +2,7 @@
 # define LIBFT_H
 
 # include <unistd.h>
+# include <stdlib.h>
 
 void	ft_putchar(char c);
 void	ft_putstr(char *s);
@@ -9,5 +10,6 @@ void	*ft_memset(void *ptr, int value, size_t num);
 void	*ft_bzero(void *ptr, size_t num);
 void	*ft_memcpy(void *dst, void *src, size_t num);
 void	*ft_memmove(void *dst, void *src, size_t num);
+void	*ft_memccpy(void *dst, void *src, int c, size_t num);
 
 #endif
