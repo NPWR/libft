9d730dd627161f99bcae2ec54dd3b1823dc2d78c
Elliot Bitsch <bitsch.elliot@gmail.com>
2015-10-12 14:35:46 +0200

    fixed stupid

--- ft_memset.c
+++ ft_memset.c
@@ -5,9 +5,9 @@ void	*ft_memset(void *ptr, int value, size_t num)
 	unsigned char	v;
 	unsigned int	i;
 
-	i = -1;
+	i = 0;
 	v = (unsigned char)value;
-	while (++i < num)
-		((unsigned char *)(ptr))[i] = v;
+	while (i < num)
+		((unsigned char *)(ptr))[i++] = v;
 	return (ptr);
 }
