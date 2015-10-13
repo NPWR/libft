29b8a5c266b3357214ab9ed4eed1c051ad826eb6
Elliot Bitsch <bitsch.elliot@gmail.com>
2015-10-13 15:10:05 +0200

    fixed prototype error. I should read the man

--- ft_bzero.c
+++ ft_bzero.c
@@ -1,11 +1,10 @@
 #include <string.h>
 
-void	*ft_bzero(void *ptr, size_t n)
+void	ft_bzero(void *ptr, size_t n)
 {
 	unsigned int	i;
 
 	i = 0;
 	while (i < n)
 		((unsigned char *)(ptr))[i++] = 0;
-	return (ptr);
 }
--- libft.h
+++ libft.h
@@ -7,7 +7,7 @@
 void	ft_putchar(char c);
 void	ft_putstr(char *s);
 void	*ft_memset(void *ptr, int value, size_t num);
-void	*ft_bzero(void *ptr, size_t num);
+void	ft_bzero(void *ptr, size_t num);
 void	*ft_memcpy(void *dst, void *src, size_t num);
 void	*ft_memmove(void *dst, void *src, size_t num);
 void	*ft_memccpy(void *dst, void *src, int c, size_t num);
