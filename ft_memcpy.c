83ac6959cef100c15c54ad653a293d7440137bdf
Elliot Bitsch <bitsch.elliot@gmail.com>
2015-10-13 15:10:33 +0200

    Handled NULL pointer paramater cases

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -5,6 +5,8 @@ void	*ft_memcpy(void *dst, void *src, size_t n)
 	unsigned int	i;
 
 	i = 0;
+	if (dst == NULL || src == NULL)
+		return (dst);
 	while (i < n)
 	{
 		((unsigned char *)(dst))[i] = ((unsigned char *)(src))[i];
