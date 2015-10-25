3e4a25d69f3c714f4d7dbe1e89b42f211b2ffa4c
root <root@vps206453.ovh.net>
2015-10-25 14:25:13 +0100

    Fixed bad prototype

--- libft.h
+++ libft.h
@@ -13,7 +13,7 @@ void	*ft_memcpy(void *dst, void *src, size_t num);
 void	*ft_memmove(void *dst, void *src, size_t num);
 void	*ft_memccpy(void *dst, void *src, int c, size_t num);
 void	*ft_memchr(void *ptr, int value, size_t num);
-int	ft_memcmp(const void *s1, const void *s2);
+int	ft_memcmp(const void *s1, const void *s2, size_t n);
 size_t	ft_strlen(const char  *s);
 char	*ft_strdup(const char *s);
 char	*ft_strcpy(char *dst, const char *src);
