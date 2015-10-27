3b3cdd360d43609c0671179ec555e3acdf080d36
Elliot Bitsch <bitsch.elliot@gmail.com>
2015-10-27 18:38:12 +0100

    oopsie

--- ft_atoi.c
+++ ft_atoi.c
@@ -4,14 +4,10 @@ int	ft_atoi(const char *s)
 	int	factor;
 
 	total = 0;
-	factor = 1;
 	while (*s == '\t' || *s == ' ' || *s == '\n')
 		s++;
-	if (*s == '-')
-	{
-		factor = -1;
-		s++;
-	}
+	s += *s == '+' ? 0 : 1;
+	factor = *s == '-' ? -1 : 1;
 	while (*s >= '0' && *s <= '9')
 	{
 		total *= 10;
