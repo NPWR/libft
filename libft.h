8ad483f3608a84b14f96595c2bb4797019f099a5
Elliot Bitsch <bitsch.elliot@gmail.com>
2015-11-05 14:08:18 +0100

    striter

--- libft.h
+++ libft.h
@@ -35,6 +35,7 @@ char	*ft_strstr(const char *haystack, const char *needle);
 char	*ft_strnstr(const char *haystack, const char *needle, size_t n);
 char	*ft_strnew(size_t size);
 void	ft_strdel(char **as);
+void	ft_striter(char *s, void(*f)(char *s));
 int	ft_strcmp(const char *s1, const char *s2);
 int	ft_strncmp(const char *s1, const char *s2, size_t n);
 int	ft_atoi(const char *s);
