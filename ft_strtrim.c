b8a2b5d9e4432a7dab734ecc2b70de6ba027d94c
Elliot Bitsch <bitsch.elliot@gmail.com>
2015-11-09 07:19:32 +0100

    safety

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -12,12 +12,12 @@ char		*ft_strtrim(char const *s)
 	char	*ptr;
 
 	new = ft_strdup(s);
-	while (is_blank(*new))
+	while (is_blank(*new) && *new)
 		new++;
 	ptr = new;
 	while (*ptr)
 		ptr++;
-	while (is_blank(*(--ptr)))
+	while (is_blank(*(--ptr)) && ptr != new)
 		*ptr = '\0';
 	return (new);
 }
