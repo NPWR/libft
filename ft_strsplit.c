bedda34bc740551256cd00dac3f5abf4e850d4ab
Elliot BITSCH <ebitsch@e2r4p17.42.fr>
2015-11-23 12:10:02 +0100

    strsplit

--- ft_strsplit.c
+++ ft_strsplit.c
@@ -5,73 +5,62 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: ebitsch <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2015/11/23 10:56:02 by ebitsch           #+#    #+#             */
-/*   Updated: 2015/11/23 11:09:56 by ebitsch          ###   ########.fr       */
+/*   Created: 2015/11/23 11:17:47 by ebitsch           #+#    #+#             */
+/*   Updated: 2015/11/23 12:00:56 by ebitsch          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <stdlib.h>
 
-static int	count_words(char *s, char c)
+static int	count_words(const char *s, char c)
 {
 	int	n;
 
-	n = 0;
+	while (*s == c)
+		++s;
 	while (*s)
 	{
-		while (*s != c && *s)
-			s++;
-		while (*s == c && *s)
-			s++;
-		n++;
+		while (*s && *s != c)
+			++s;
+		while (*s && *s == c)
+			++s;
+		++n;
 	}
 	return (n);
 }
 
-static int	wordlen(char *s, char c)
+static int	wordlen(const char *s, char c)
 {
 	char	*ptr;
 
-	ptr = s;
+	ptr = (char*)s;
 	while (*ptr && *ptr != c)
-		ptr++;
+		++ptr;
 	return ((int)(ptr - s));
-}
-
-static char	*clean(const char *s, char c)
-{
-	char	*new;
-
-	new = (char*)s;
-	while (*new == c && *new)
-		new++;
-	return (new);
 }
 
 char		**ft_strsplit(const char *s, char c)
 {
 	char	**tab;
-	char	*copy;
 	int		word;
 
-	copy = clean(s, c);
-	tab = (char**)malloc(sizeof(char*) * (count_words(copy, c) + 1));
-	if (tab == NULL) return (NULL);
+	if (!(tab = malloc(sizeof(char*) * (count_words(s, c) + 1))))
+		return (NULL);
 	word = 0;
-	while (*copy)
+	while (*s && *s == c)
+		++s;
+	while (*s)
 	{
-		tab[word] = (char*)malloc(sizeof(char) * (wordlen(copy, c) + 1));
-		if (tab[word] == NULL) return (NULL);
-		ft_strncpy(tab[word], copy, wordlen(copy, c));
-		tab[word][wordlen(copy, c)] = '\0';
-		copy += *copy ? wordlen(copy, c) : 0;
-		while (*copy && *copy == c)
-			copy++;
-		word++;
+		if (!(tab[word] = ft_strsub(s, 0, wordlen(s, c))))
+			return (NULL);
+		s += wordlen(s, c);
+		while (*s && *s == c)
+			++s;
+		++word;
 	}
-	tab[word] = (char*)malloc(sizeof(char));
-	if (tab[word] == NULL) return (NULL);
+	if (!(tab[word] = malloc(sizeof(char))))
+		return (NULL);
 	tab[word][0] = '\0';
 	return (tab);
 }
