b8e05807a16adf533df5a0e1070f1e0f6a09842a
Elliot BITSCH <ebitsch@e2r7p15.42.fr>
2015-11-25 00:36:32 +0100

    fixed strlcat

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: ebitsch <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/11/23 10:54:52 by ebitsch           #+#    #+#             */
-/*   Updated: 2015/11/24 13:19:10 by ebitsch          ###   ########.fr       */
+/*   Updated: 2015/11/25 00:36:16 by ebitsch          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,18 @@ size_t	ft_strlcat(char *dst, const char *src, size_t n)
 {
 	size_t	src_len;
 	size_t	dst_len;
+	size_t	i;
 
 	src_len = ft_strlen(src);
 	dst_len = ft_strlen(dst);
-	if (src_len + dst_len >= n - 1)
-		return (src_len + dst_len);
-	memcpy(dst + dst_len, src, src_len + 1);
+	if (dst_len >= n)
+		return (n + src_len);
+	i = dst_len;
+	while (i < (n - 1) && src[i - dst_len])
+	{
+		dst[i] = src[i - dst_len];
+		++i;
+	}
+	dst[i] = '\0';
 	return (src_len + dst_len);
 }
