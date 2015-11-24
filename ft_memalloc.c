962c47239087ea1e54015173f5ba5abb2c12d013
Elliot BITSCH <ebitsch@e1r4p15.42.fr>
2015-11-24 13:35:42 +0100

    line opti

--- ft_memalloc.c
+++ ft_memalloc.c
@@ -6,7 +6,7 @@
 /*   By: ebitsch <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/11/23 10:52:32 by ebitsch           #+#    #+#             */
-/*   Updated: 2015/11/23 10:52:48 by ebitsch          ###   ########.fr       */
+/*   Updated: 2015/11/24 13:35:27 by ebitsch          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,7 @@ void	*ft_memalloc(size_t size)
 {
 	void	*zone;
 
-	zone = (void*)malloc(sizeof(unsigned char) * size);
-	if (zone == NULL)
+	if (!(zone = (void*)malloc(sizeof(unsigned char) * size)))
 		return (NULL);
 	ft_memset(zone, 0, size);
 	return (zone);
