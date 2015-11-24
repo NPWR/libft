bad556b046a80a2076b775611cf7d9f8e0dbd623
Elliot BITSCH <ebitsch@e1r4p15.42.fr>
2015-11-24 14:05:49 +0100

    ft_strcmp

--- ft_strcmp.c
+++ ft_strcmp.c
@@ -6,18 +6,19 @@
 /*   By: ebitsch <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/11/23 10:54:13 by ebitsch           #+#    #+#             */
-/*   Updated: 2015/11/24 13:24:29 by ebitsch          ###   ########.fr       */
+/*   Updated: 2015/11/24 14:05:22 by ebitsch          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <string.h>
+#include "libft.h"
 
 int	ft_strcmp(const char *s1, const char *s2)
 {
-	while (*s1 && *s1 == *s2)
-	{
-		s1++;
-		s2++;
-	}
-	return (*s1 - *s2);
+	size_t	l1;
+	size_t	l2;
+
+	l1 = ft_strlen(s1);
+	l2 = ft_strlen(s2);
+	return (ft_memcmp(s1, s2, l1 > l2 ? l2 + 1 : l1 + 1));
 }
