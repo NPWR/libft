a947c07d0448fa64f5733fa88053b2c90413ecb3
Elliot BITSCH <ebitsch@e1r4p15.42.fr>
2015-11-24 13:31:44 +0100

    cleaning

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,28 +6,22 @@
 /*   By: ebitsch <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/11/23 10:54:52 by ebitsch           #+#    #+#             */
-/*   Updated: 2015/11/23 10:54:53 by ebitsch          ###   ########.fr       */
+/*   Updated: 2015/11/24 13:19:10 by ebitsch          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <string.h>
+#include "libft.h"
 
 size_t	ft_strlcat(char *dst, const char *src, size_t n)
 {
-	unsigned int	i;
-	unsigned int	j;
+	size_t	src_len;
+	size_t	dst_len;
 
-	i = 0;
-	while (dst[i])
-		i++;
-	j = 0;
-	while (src[j] && i + j < n - 1)
-	{
-		dst[i + j] = src[j];
-		j++;
-	}
-	dst[i + j] = '\0';
-	while (src[j])
-		j++;
-	return (i + j);
+	src_len = ft_strlen(src);
+	dst_len = ft_strlen(dst);
+	if (src_len + dst_len >= n - 1)
+		return (src_len + dst_len);
+	memcpy(dst + dst_len, src, src_len + 1);
+	return (src_len + dst_len);
 }
