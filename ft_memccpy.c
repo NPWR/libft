870c51a311a4b475a74bdfaff9bde4c8ed537fe5
Elliot BITSCH <ebitsch@e1r4p15.42.fr>
2015-11-24 15:25:43 +0100

    returns the character one byte AFTER the found character

--- ft_memccpy.c
+++ ft_memccpy.c
@@ -6,7 +6,7 @@
 /*   By: ebitsch <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/11/23 10:52:50 by ebitsch           #+#    #+#             */
-/*   Updated: 2015/11/23 10:52:53 by ebitsch          ###   ########.fr       */
+/*   Updated: 2015/11/24 15:24:38 by ebitsch          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,11 @@ void	*ft_memccpy(void *dst, void *src, int c, size_t n)
 	unsigned int	i;
 
 	i = 0;
-	if (dst == NULL || src == NULL)
-		return (NULL);
 	while (i < n)
 	{
 		((unsigned char *)(dst))[i] = ((unsigned char *)(src))[i];
 		if (((unsigned char *)(dst))[i] == (unsigned char)(c))
-			return ((unsigned char *)(dst) + i);
+			return ((unsigned char *)(dst) + i + 1);
 		i++;
 	}
 	return (NULL);
